febc4562d85f873dd8a38ed6efe1aa2916b88c1f
Mauricio Patiño <mapal0925@gmail.com>
2026-06-09 18:53:34 -0500

    feat(routing): configurar rutas de autenticacion, dashboard y recuperacion
    - "/" redirige a /login
    - /login y /recovery usan AuthLayout
    - /dashboard usa DashboardLayout y queda protegido por authGuard
    - Cualquier ruta no definida redirige a /login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,54 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/auth/auth.guard';
+/* Autenticación  */
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { LoginComponent } from './features/auth/login/login.component';
+import { PasswordRecoveryComponent } from './features/password-recovery/password-recovery.component';
+/* Dashboard  */
+import { DashboardLayoutComponent } from './layouts/dashboard-layout/dashboard-layout.component';
+import { DashboardHomeComponent } from './features/dashboard/pages/home/dashboard-home.component';
 
-export const routes: Routes = [];
+/**
+  URL "/"          → redirige a /login
+  URL "/login"     → AuthLayout > LoginComponent
+  URL "/dashboard" → DashboardLayout > DashboardHome  (protegida por authGuard)
+  URL "/cualquier" → redirige a /login
+*/
+
+export const routes: Routes = [
+  {
+    path: '', // URL: "/" -> '' URL RAÍZ
+    redirectTo: 'login', // Redirige automáticamente a "/login"
+    pathMatch: 'full'
+  },
+  {
+    path: '', // URLs de autenticación
+    component: AuthLayoutComponent, // Usa el layout de auth como contenedor
+    children: [
+      {
+        path: 'login', // URL: "/login"
+        component: LoginComponent // Muestra el formulario de login
+      },
+      {
+        path: 'recovery', // URL: "/login"
+        component: PasswordRecoveryComponent // Muestra el formulario de login
+      }
+    ]
+  },
+  {
+    path: '', // URLs del panel admin
+    component: DashboardLayoutComponent, // Usa el layout de dashboard
+    canActivate: [authGuard], // Solo accesible si el usuario está logueado
+    children: [
+      {
+        path: 'dashboard', // URL: "/dashboard"
+        component: DashboardHomeComponent // Muestra el dashboard
+      },
+    ]
+  },
+  {
+    path: '**', // Cualquier URL que no exista
+    redirectTo: 'login' // Redirige a login (página 404 implícita)
+  }
+];
+
