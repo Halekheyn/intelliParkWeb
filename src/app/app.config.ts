15e0b4cbdda354c62ee6a19e690962dd2e23d6e1
Mauricio Patiño <mapal0925@gmail.com>
2026-06-09 20:01:17 -0500

    feat(app-shell): habilitar HttpClient en la configuracion global
    - Agrega provideHttpClient para permitir peticiones HTTP a la API

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+
+import { provideHttpClient } from '@angular/common/http';
+
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(), // Captura errores globales del navegador
     provideZonelessChangeDetection(), // Detección de cambios sin Zone.js
-    provideRouter(routes) // Registra el sistema de rutas - Importante
+    provideRouter(routes), // Registra el sistema de rutas - Importante
+    provideHttpClient() // Permite hacer peticiones HTTP.
   ]
 };
